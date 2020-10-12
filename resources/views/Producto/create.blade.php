1727ac8cf82c705e73c5c478d1b530eca721a3b0
HeberZabdiel <hzhm1997@gmail.com>
2020-10-12 10:44:41 -0500

    MVCdeProducto

--- resources/views/Producto/create.blade.php
+++ resources/views/Producto/create.blade.php
@@ -2,5 +2,9 @@
     {{ csrf_field() }}
     <label for="Nombre">{{'Nombre'}}</label>
     <input type="text" name="Nombre" id="Nombre" value="">
+    <label for="Descripcion">{{'Descripcion'}}</label>
+    <input type="text" name="Descripcion" id="Descripcion" value="">
+    <label for="MinimoStock">{{'Minimo Stock'}}</label>
+    <input type="text" name="MinimoStock" id="MinimoStock" value="">
     <input type="submit" value="Agregar">
 </form>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\ProductoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('producto', ProductoController::class);
+
