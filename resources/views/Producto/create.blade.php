2582147cfecb589c5c32b12dbf8afc7b25d31b3d
HeberZabdiel <hzhm1997@gmail.com>
2020-10-16 12:29:07 -0500

    cambioviewproducto

--- resources/views/Producto/create.blade.php
+++ resources/views/Producto/create.blade.php
@@ -7,5 +7,6 @@
     <label for="MinimoStock">{{'Minimo Stock'}}</label>
     <input type="text" name="minimo_stock" id="minimo_stock" value="">
     <input type="number" name="existencia" id="existencia" value="0" style="display:none">
+    
     <input type="submit" value="Agregar">
 </form>
