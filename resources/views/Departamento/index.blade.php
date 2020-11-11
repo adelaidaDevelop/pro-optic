2b0097185662f0020ca4df99a24bc6ac47ffd9a6
HeberZabdiel <hzhm1997@gmail.com>
2020-11-10 23:03:11 -0600

    cambiosDepa

--- resources/views/Departamento/index.blade.php
+++ resources/views/Departamento/index.blade.php
@@ -6,10 +6,11 @@
     <title>Document</title>
     <!--link rel="stylesheet" href="https://unpkg.com/@popperjs/core@2" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"-->
     <!--link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"-->
-    <script src="{{ asset('js\jquery-3.5.1.min.js') }}"></script>
-    <script src="{{ asset('js\popper.min.js') }}"></script>
-    <script src="{{ asset('js\bootstrap.min.js') }}"></script>
     <link rel="stylesheet" href="{{ asset('css\bootstrap.min.css') }}">
+    <script href="{{ asset('js\jquery-3.5.1.min.js') }}"></script>
+    <script href="{{ asset('js\popper.min.js') }}"></script>
+    <script href="{{ asset('js\bootstrap.min.js') }}"></script>
+    
 </head>
 <body>
 
@@ -18,13 +19,13 @@
         <h1>DEPARTAMENTOS</h1>
     </div>
     <div class="row">
-        <div class="col" style="background:#0CC6CC">
+        <div class="col-4" style="background:#0CC6CC">
+            <div class="row">
+            <input type="text" id="buscador" class="form-control my-2">
+            <button class="btn btn-info mb-2" id="boton">Buscar</button>
+            </div>
+            <div class="row">
             <table class="table table-striped">
-                <thead>
-                    <!--tr>
-                        <th>Departamento</th>
-                    </tr-->
-                </thead>
                 <tbody>
                 <nav id="navbar-example2" class="navbar navbar-light bg-light">
                 
@@ -75,6 +76,7 @@
                 </div>
                 </tbody>
             </table>
+            </div>
         </div>
         <div class="col" style="background:#FFFBF2">
         @if(isset($d))
@@ -114,5 +116,24 @@
         </div>
     </div>
 </div>
+<script>
+    const departamentos = [
+        {nombre: 'Venta Libre', valor:500},
+        {nombre: 'perfumeria', valor:500},
+        {nombre: 'patente', valor:500},
+        {nombre: 'Papeleria', valor:500},
+    ]
+    const buscador = document.querySelector('#buscador');
+    const boton = document.querySelector('#boton');
+
+    const filtrar = ()=>{
+        console.log($departamentos);
+        //const texto = buscador.value.toLowerCase();
+        //for(let producto of departamentos)
+    }
+
+    boton.addEventListener('click',filtrar);
+
+</script>
 </body>
 </html>
