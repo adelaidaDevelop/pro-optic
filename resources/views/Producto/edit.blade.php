86379da63f95f152129d0e6e54d013c557f4e74d
Adelaida23 <40848191+Adelaida23@users.noreply.github.com>
2020-11-10 20:51:46 -0600

    Formulario crear y editar producto compartido y existencia quitado

--- resources/views/Producto/edit.blade.php
+++ resources/views/Producto/edit.blade.php
@@ -2,45 +2,5 @@
 <form method="post" action="{{url('/producto/'.$producto->id)}}" enctype="multipart/form-data">
     {{ csrf_field() }}
     {{method_field('PATCH')}}
-   
-    <label for="idDepartamento">{{'Departamento'}}</label>  
-    <select name="idDepartamento" id="idDepartamento">
-    @foreach($departamento as $departamento)
-    <!--Hacer un if para asignar el selected-->
-    <option value="{{ $departamento['id']}}"> {{$departamento['nombre']}}</option>
-    @endforeach
-    </select>
-
-    <br/>
-
-    <label for="codigoBarras">{{'CodigoBarras'}}</label>  
-    <!--El name debe ser igual al de la base de datos-->      |
-    <input type="text" name="codigoBarras" id="codigoBarras" value=" {{$producto->codigoBarras}}">
-    <br/>
-
-    <label for="Nombre">{{'Nombre'}}</label>        |
-    <input type="text" name="nombre" id="nombre" value="{{ $producto->nombre}}">
-    <br/>
-
-    <label for="Descripcion">{{'Descripcion'}}</label>
-    <input type="text" name="descripcion" id="descripcion" value="{{ $producto->descripcion}}">
-    <br/>
-
-    <label for="Imagen">{{'Imagen'}}</label>
-   <!-- {{ $producto->imagen}}-->
-    <img src="{{ asset('storage').'/'.$producto->imagen}}" alt="" width="200">
-    <input type="file" name="Imagen" id="Imagen" value="{{ $producto->imagen}}">
-    <br/>
-
-    <label for="MinimoStock">{{'Minimo Stock'}}</label>
-    <input type="number" name="minimo_stock" id="minimo_stock" value="{{ $producto->minimo_stock}}">
-    <br/>
-
-    <label for="existencia">{{'Existencia'}}</label>
-    <input type="number" name="existencia" id="existencia" value="{{ $producto->existencia}}" disabled >
-  <br/>
-
-    <input type="submit" value="Editar producto">
-    
-    <a href="{{url('producto')}}"> Regresar</a>
+    @include('Producto.form', ['Modo' => 'editar'])
 </form>
--- resources/views/Producto/form.blade.php
+++ resources/views/Producto/form.blade.php
@@ -9,31 +9,35 @@
     </select>
 
     <label for="codigoBarras">{{'CodigoBarras'}}</label>  
-    <!--El name debe ser igual al de la base de datos-->      |
-    <input type="text" name="codigoBarras" id="codigoBarras" value="">
+    <!--El name debe ser igual al de la base de datos-->      
+    <input type="text" name="codigoBarras" id="codigoBarras" 
+    value="{{ isset($producto->codigoBarras)?$producto->codigoBarras:''}}">
     <br/>
 
     <label for="Nombre">{{'Nombre'}}</label>        |
-    <input type="text" name="nombre" id="nombre" value="">
+    <input type="text" name="nombre" id="nombre" 
+    value=" {{ isset($producto->nombre)?$producto->nombre:''}} ">
     <br/>
 
     <label for="Descripcion">{{'Descripcion'}}</label>
-    <input type="text" name="descripcion" id="descripcion" value="">
+    <input type="text" name="descripcion" id="descripcion" 
+    value=" {{ isset($producto->descripcion)?$producto->descripcion:''}}">
     <br/>
 
     <label for="Imagen">{{'Imagen'}}</label>
-    
+    @if(isset($producto->imagen))
+    <br/>
+    <img src="{{ asset('storage').'/'.$producto->imagen}}" alt="" width="200">
+    <br/>
+    @endif
     <input type="file" name="Imagen" id="Imagen" value="">
     <br/>
 
     <label for="MinimoStock">{{'Minimo Stock'}}</label>
-    <input type="number" name="minimo_stock" id="minimo_stock" value="">
+    <input type="number" name="minimo_stock" id="minimo_stock" 
+    value=" isset($producto->minimo_stock)?$producto->minimo_stock:''">
     <br/>
-
-    <label for="existencia">{{'Existencia'}}</label>
-    <input type="number" name="existencia" id="existencia" value="0" disabled >
   <br/>
-
-    <input type="submit" value="Agregar producto">
+    <input type="submit" value=" {{ $Modo== 'crear' ?'Agregar' : 'Editar' }}">
     <br/>
     <a href="{{url('producto')}}"> Regresar</a>
