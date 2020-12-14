7b6955e3ffb54d0e713abb7bd42b50802bbd1367
HeberZabdiel <hzhm1997@gmail.com>
2020-12-13 22:02:47 -0600

    actualización cabecera

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,79 +10,82 @@
     <script src="{{ asset('js\popper.min.js') }}"></script>
     <script src="{{ asset('js\bootstrap.min.js') }}"></script>
 </head>
-<body>
-<div class="container-fluid">
-<nav class="navbar navbar-expand-lg navbar-light w-100" style="background-color:#3366FF;">
-  <a class="navbar-brand" href="#">
-    <img src="{{ asset('img\farmaciagilogo.png') }}" alt="Editar"  height="50px">         
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#"> 
-            <button class="btn btn-light">
-                VENTAS
-            </button> 
-        <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#"> 
-            <button class="btn btn-light">
-                COMPRAS
-            </button> 
-        <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#"> 
-            <button class="btn btn-light">
-                PRODUCTOS
-            </button> 
-        <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#"> 
-            <button class="btn btn-light">
-                INVENTARIO
-            </button> 
-        <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#"> 
-            <button class="btn btn-light">
-                EMPLEADOS
-            </button> 
-        <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#"> 
-            <button class="btn btn-light">
-                CLIENTES
-            </button> 
-        <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#"> 
-            <button class="btn btn-light">
-                CORTE
-            </button> 
-        <span class="sr-only">(current)</span></a>
-      </li>
-    </ul>
-    <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-            <a class="nav-link" href="#"> 
-                <img src="{{ asset('img\salir.png') }}" alt="Editar"  height="30px">   
-                <span class="sr-only">(current)</span>
+<body>
+    <div class="container-fluid align-self-center">
+        <nav class="navbar navbar-expand-lg navbar-light w-100 "
+            style="height: 100px;background-color:#3366FF;">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('img\farmaciagilogo.png') }}" alt="Editar" height="50px">
             </a>
-        </li>
-    </ul>
-  </div>
-</nav>
-</div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">
+                            <button class="btn btn-light">
+                                VENTAS
+                            </button>
+                            <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">
+                            <button class="btn btn-light">
+                                COMPRAS
+                            </button>
+                            <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">
+                            <button class="btn btn-light">
+                                PRODUCTOS
+                            </button>
+                            <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">
+                            <button class="btn btn-light">
+                                INVENTARIO
+                            </button>
+                            <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">
+                            <button class="btn btn-light">
+                                EMPLEADOS
+                            </button>
+                            <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">
+                            <button class="btn btn-light">
+                                CLIENTES
+                            </button>
+                            <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">
+                            <button class="btn btn-light">
+                                CORTE
+                            </button>
+                            <span class="sr-only">(current)</span></a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <img src="{{ asset('img\salir.png') }}" alt="Editar" height="30px">
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
     <!--div class="row" style="background:#4388CC">
         <div class="row m-4">
             <div class="col-3">
@@ -130,4 +134,5 @@
         </div>                
     </div-->
 </body>
+
 </html>
