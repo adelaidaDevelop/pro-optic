5b7988cecf385b2524a2b50579a2ead606373a2e
Adelaida23 <40848191+Adelaida23@users.noreply.github.com>
2020-12-19 11:09:06 -0600

    CABECERA

--- resources/views/Producto/edit.blade.php
+++ resources/views/Producto/edit.blade.php
@@ -1,22 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css\bootstrap.min.css') }}">
-    <script src="{{ asset('js\jquery-3.5.1.min.js') }}"></script>
-    <script src="{{ asset('js\popper.min.js') }}"></script>
-    <script src="{{ asset('js\bootstrap.min.js') }}"></script>
-</head>
-
-<body>
     <form method="post" action="{{url('/producto/'.$producto->id)}}" enctype="multipart/form-data">
         {{ csrf_field() }}
         {{method_field('PATCH')}}
                 @include('Producto.form', ['Modo' => 'editar'])
     </form>
-</body>
-
-</html>
--- resources/views/header2.blade.php
+++ resources/views/header2.blade.php
@@ -1,96 +1,96 @@
+@extends('layouts.app')
+@section('content')
 
-        @extends('layouts.app')
-        @section('content')
-     
-            <div class="container-fluid">
-                <div class="row" style="background:#3366FF">
-                    <div class="container-fluid align-self-center">
-                    <nav class="navbar navbar-expand-lg navbar-light w-100 " style="height: 50px;background-color:#3366FF;">
+<div class="container-fluid">
+    <div class="row" style="background:#3366FF">
+        <div class="container-fluid align-self-center">
+            <nav class="navbar navbar-expand-lg navbar-light w-100 " style="height: 50px;background-color:#3366FF;">
 
-                        
-                            <a class="navbar-brand" href="#">
-                                <img src="{{ asset('img\farmaciagilogo.png') }}" alt="Editar" height="50px">
+
+                <a class="navbar-brand" href="#">
+                    <img src="{{ asset('img\farmaciagilogo.png') }}" alt="Editar" height="50px">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">
+                                <button class="btn btn-light">
+                                    VENTAS
+                                </button>
+                                <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">
+                                <button class="btn btn-light">
+                                    COMPRAS
+                                </button>
+                                <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">
+                                <button class="btn btn-light">
+                                    PRODUCTOS
+                                </button>
+                                <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">
+                                <button class="btn btn-light">
+                                    INVENTARIO
+                                </button>
+                                <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">
+                                <button class="btn btn-light">
+                                    EMPLEADOS
+                                </button>
+                                <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">
+                                <button class="btn btn-light">
+                                    CLIENTES
+                                </button>
+                                <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">
+                                <button class="btn btn-light">
+                                    CORTE
+                                </button>
+                                <span class="sr-only">(current)</span></a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <img src="{{ asset('img\salir.png') }}" alt="Editar" height="30px">
+                                <span class="sr-only">(current)</span>
                             </a>
-                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="navbar-toggler-icon"></span>
-                            </button>
-
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul class="navbar-nav mr-auto">
-                                    <li class="nav-item active">
-                                        <a class="nav-link" href="#">
-                                            <button class="btn btn-light">
-                                                VENTAS
-                                            </button>
-                                            <span class="sr-only">(current)</span></a>
-                                    </li>
-                                    <li class="nav-item active">
-                                        <a class="nav-link" href="#">
-                                            <button class="btn btn-light">
-                                                COMPRAS
-                                            </button>
-                                            <span class="sr-only">(current)</span></a>
-                                    </li>
-                                    <li class="nav-item active">
-                                        <a class="nav-link" href="#">
-                                            <button class="btn btn-light">
-                                                PRODUCTOS
-                                            </button>
-                                            <span class="sr-only">(current)</span></a>
-                                    </li>
-                                    <li class="nav-item active">
-                                        <a class="nav-link" href="#">
-                                            <button class="btn btn-light">
-                                                INVENTARIO
-                                            </button>
-                                            <span class="sr-only">(current)</span></a>
-                                    </li>
-                                    <li class="nav-item active">
-                                        <a class="nav-link" href="#">
-                                            <button class="btn btn-light">
-                                                EMPLEADOS
-                                            </button>
-                                            <span class="sr-only">(current)</span></a>
-                                    </li>
-                                    <li class="nav-item active">
-                                        <a class="nav-link" href="#">
-                                            <button class="btn btn-light">
-                                                CLIENTES
-                                            </button>
-                                            <span class="sr-only">(current)</span></a>
-                                    </li>
-                                    <li class="nav-item active">
-                                        <a class="nav-link" href="#">
-                                            <button class="btn btn-light">
-                                                CORTE
-                                            </button>
-                                            <span class="sr-only">(current)</span></a>
-                                    </li>
-                                </ul>
-                                <ul class="navbar-nav ml-auto">
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="#">
-                                            <img src="{{ asset('img\salir.png') }}" alt="Editar" height="30px">
-                                            <span class="sr-only">(current)</span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </nav>
-                    </div>
-
-
+                        </li>
+                    </ul>
                 </div>
-                <div class="row" style="background:#ED4D46">
-                    <h3 class="font-weight-bold my-2 ml-4 px-1" style="color:#FFFFFF">
-                        @yield('subtitulo')
-                    </h3>
-                    @yield('opciones')
-                </div>
-                <!--BODY-->
-                @yield('contenido')
-            </div>
+               
+            </nav>
+            <br/>
+        </div>
+    
 
-  
-        @endsection
-  
+    </div>
+    <div class="row" style="background:#ED4D46">
+        <h3 class="font-weight-bold my-2 ml-4 px-1" style="color:#FFFFFF">
+            @yield('subtitulo')
+        </h3>
+        @yield('opciones')
+    </div>
+    <!--BODY-->
+    @yield('contenido')
+</div>
+
+
+@endsection
