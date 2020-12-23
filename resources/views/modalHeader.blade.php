f7e1e46ef94c690f45fd6003712bd06e19a23de2
Adelaida23 <40848191+Adelaida23@users.noreply.github.com>
2020-12-22 19:04:46 -0600

    Vista modalHeader

---
+++ resources/views/modalHeader.blade.php
@@ -0,0 +1,23 @@
+
+<div class="container-fluid">
+    <div class="row" style="background:#3366FF">
+        <div class="container-fluid align-self-center">
+            <nav class="navbar navbar-expand-lg navbar-light w-100 " style="height: 20px;background-color:#3366FF;">
+                <h6 class="font-weight-bold my-2 ml-4 px-1" style="color:#FFFFFF">
+                    @yield('titulo')
+                </h6>
+
+            </nav>
+            <br />
+        </div>
+    </div>
+
+    <div class="row" style="background:#ED4D46">
+        <h6 class="font-weight-bold my-2 ml-4 px-1" style="color:#FFFFFF">
+            @yield('subtitulo')
+
+        </h6>
+
+    </div>
+    <!--BODY-->
+</div>
