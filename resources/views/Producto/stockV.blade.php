61d6d15da0768e32a6e4171b06e8771c4f705806
Adelaida23 <40848191+Adelaida23@users.noreply.github.com>
2021-02-19 11:31:09 -0600

    Solo comentar la linea del controlador sucursal producto para que no arroge error

--- resources/views/Producto/stockV.blade.php
+++ resources/views/Producto/stockV.blade.php
@@ -9,37 +9,99 @@ PRODUCTOS
 
 <form method="post" action="{{url('/puntoVenta/sucursalProducto')}}" enctype="multipart/form-data">
     {{ csrf_field() }}
-     <!-- TABLA -->
-     <div class="row" style="height:350px;overflow-y:auto;">
-                    <table class="table table-bordered border-primary col-12 " id="productos">
-                        <thead class="table-secondary text-primary">
+    <!-- TABLA -->
+    <div class="row" style="height:350px;overflow-y:auto;">
+        <table class="table table-bordered border-primary col-12 " id="productos">
+            <thead class="table-secondary text-primary">
 
-                            <tr>
-                                <th>#</th>
-                                <th>CODIGO BARRAS</th>
-                                <th>NOMBRE</th>
-                                <th>EXISTENCIA</th>
-                                <th>DEPARTAMENTO</th>
-                                <th>COSTO</th>
-                                <th>PRECIO</th>
-                                <th>ACCIONES</th>
-                            </tr>
-                        </thead>
-                        <tbody id="consultaBusqueda" class="text-uppercase ">
+                <tr>
+                    <th>#</th>
+                    <th>CODIGO BARRAS</th>
+                    <th>NOMBRE</th>
+                    <th>EXISTENCIA</th>
+                    <th>DEPARTAMENTO</th>
+                    <th>COSTO</th>
+                    <th>PRECIO</th>
+                    <th>ACCIONES</th>
+                </tr>
+            </thead>
+            <tbody id="consultaBusqueda" class="text-uppercase ">
 
-                        </tbody>
-                    </table>
-                </div>
+            </tbody>
+        </table>
+    </div>
 </form>
 
 <script>
-let productos = @json('productos');
-let deptos = @json('depa');
+    let productos = @json('productos');
+    let deptos = @json('depa');
 
-function cargarProductos(){
-    
-}
+    function cargarProductos() {
+        let cuerpo = "";
+        let contador = 0;
+        let departamento="";
+        for (let t in productos) {
+            for (count in deptos) {
+                if (productos[t].idDepartamento === deptos[count].id) {
+                    departamento = deptos[count].nombre;
+                }
+            }
 
+            cuerpo = cuerpo + `
+                 <tr onclick="" data-dismiss="modal">
+                 <th scope="row">` + contador + `</th>
+                   <td>` + productos[t].codigoBarras + `</td>
+                   <td>` + productos[t].nombre + `</td>
+                   <td>` + departamento + `</td>
+                    <td>` +
+                ` <button type="button" class="btn btn-outline-info" data-toggle="modal" href=".bd-example-modal-lg" id="ver" onclick=" return info4( ` + productos[t].id + `)" value="` + productos[t].id + `">
+                                agregar
+                                </button>
+                                </td>            
+                            </tr>
+                            `;
+        }
+        document.getElementById("consultaBusqueda").innerHTML = cuerpo;
+    }
+
+    cargarProductos();
+
+    function rellenar() {
+        let cuerpo = "";
+        let contador = 0;
+        let departamento = "";
+        for (let t in productosList) {
+            console.log("prod list");
+            for (let z in productosSucursal) {
+                if (productosList[t].id === productosSucursal[z].idProducto) {
+                    if (productosSucursal[z].status === 1) {
+                        for (count8 in d) {
+                            if (productosList[t].idDepartamento === d[count8].id) {
+                                departamento = d[count8].nombre;
+                            }
+                        }
+                        cuerpo = cuerpo + `
+                            <tr onclick="" data-dismiss="modal">
+                                <th scope="row">` + contador + `</th>
+                                <td>` + productosList[t].codigoBarras + `</td>
+                                <td>` + productosList[t].nombre + `</td>
+                                <td>` + productosList[t].existencia + `</td>
+                                <td>` + departamento + `</td>
+                                <td>` + productosSucursal[z].costo + `</td>
+                                <td>` + productosSucursal[z].precio + `</td>
+                                <td>` +
+                            ` <button type="button" class="btn btn-outline-info" data-toggle="modal" href=".bd-example-modal-lg" id="ver" onclick=" return info4( ` + productosList[t].id + `)" value="` + productosList[t].id + `">
+                                VER MAS
+                                </button>
+                                </td>            
+                            </tr>
+                            `;
+                    }
+                }
+            }
+        }
+        document.getElementById("consultaBusqueda").innerHTML = cuerpo;
+    };
 </script>
 
 @endsection
--- routes/puntoVenta.php
+++ routes/puntoVenta.php
@@ -40,7 +40,7 @@ Route::prefix('/puntoVenta')->group(function()
     //AGREGAR PRODUCTOS A SUCURSAL DESDE STOCK
     Route::get('/producto/stock', [ProductoController::class,'stock']);
     //Sucursal producto
-    Route::get('sucursalProducto', SucursalProductoController::class);
+   // Route::get('sucursalProducto', SucursalProductoController::class);
 
 
     Route::get('productoEli/{id}', function($id){
