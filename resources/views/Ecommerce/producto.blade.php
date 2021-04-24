d53b5555da7930dae144df412af52050b0a8e70d
HeberZabdiel <hzhm1997@gmail.com>
2021-04-24 14:53:19 -0500

    actualizacion Ecommerce/producto.blade.php

--- resources/views/Ecommerce/producto.blade.php
+++ resources/views/Ecommerce/producto.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.headerEcommerce')
 @section('contenido')
 <div class="row col-12 my-5">
-    <div class="row col-md-8 p-2 mx-auto" >
+    <div class="row col-md-7 p-2 mx-auto" >
     @if(!empty($producto->imagen))
-    <img src="{{ asset('storage').'/'.$producto->imagen}}" alt="" 
+    <img src="{{ asset('storage').'/'.$producto->imagen}}" alt="" height="400" 
             class="mx-auto mb-0 p-0">
     @else
       <img src="{{ asset('img/imagenNoDisponible.jpg') }}" alt="" height="400" 
             class="mx-auto mb-0 p-0">
     @endif
     </div>
-    <div class="form-group col-md-4 ml-auto py-2">
+    <div class="form-group col-md-5 ml-auto py-2">
         <h4 class="text-uppercase text-primary">{{$producto->nombre}}</h4>
         <h2>$ {{$producto->precio}}</h2>
         <div class="form-group">
@@ -19,14 +19,14 @@
           <p class="my-0"><small>* Descuento ya incluído en precios mostrados.</small></p>
         </div>
         <div class="form-group col-5 pl-0 pr-4">
-            <input type="number" class="form-control border">
+          <input type="number" class="form-control border" id="cantidad" min="1" max="{{$producto->existencia}}" value="1"> 
         </div>
         <button class="btn btn-success"><strong><h4>Agregar al carrito</h4></strong></button>
     </div>
 </div>
 <ul class="nav nav-tabs" id="myTab" role="tablist">
   <li class="nav-item" role="presentation">
-    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
+    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Descripcion</a>
   </li>
   <li class="nav-item" role="presentation">
     <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
@@ -36,7 +36,7 @@
   </li>
 </ul>
 <div class="tab-content" id="myTabContent">
-  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">homes.</div>
+  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">{{$producto->descripcion}}</div>
   <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">perfiles</div>
   <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">contactoos</div>
 </div>
