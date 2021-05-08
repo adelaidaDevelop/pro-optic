4dec491a19d135b0394b3c2bed97a742193cdcf2
HeberZabdiel <hzhm1997@gmail.com>
2021-05-08 12:23:31 -0500

    actualizacion Ecommerce/domicilio.blade.php

--- resources/views/Ecommerce/domicilio.blade.php
+++ resources/views/Ecommerce/domicilio.blade.php
@@ -4,7 +4,7 @@
     aqui va el proceso de compra
 </div>
 <div class="row col-12 mx-auto">
-    <div class="row col-9">
+    <div class="col-9">
         <div class="row col-12 mx-0 mb-auto border-bottom border-dark">
             <div class="col-1 my-1 mx-0 p-0">
                 <img class="row col-10 img-fluid ml-auto" src="{{ asset('img\ubicacion.png') }}" alt="UBICACION" />
@@ -12,11 +12,12 @@
             <h5 class="col-auto my-auto text-left">Dirección De Envío</h5>
         </div>
         <div class="row col-12 mt-0 mx-auto  mb-auto">
-            <p class="col-12">Indica la dirección donde quieres recibir tu compra</p>
+            <p class="col-auto mr-auto">Indica la dirección donde quieres recibir tu compra</p>
+            <p class="col-auto ml-auto font-italic text-danger">*Campos requeridos</p>
         </div>
         <div class="row col-12 mx-auto px-0">
             <form class="row col-12 mx-auto">
-                <div class="form-row col-12 mx-auto">
+                <div class="form-row col-12 mx-auto px-0">
                     <div class="form-group col-6">
                         <label class="row mx-auto my-0"for="exampleInputEmail1"><p class="text-danger m-0 mr-1">*</p> Calle</label>
                         <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
@@ -36,7 +37,7 @@
                             else.</small-->
                     </div>
                 </div>
-                <div class="form-row col-12 mx-auto">
+                <div class="form-row col-12 mx-auto px-0">
                     <div class="form-group col-6">
                     <label class="row mx-auto my-0"for="exampleInputEmail1"><p class="text-danger m-0 mr-1">*</p>Código Postal</label>
                         <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
@@ -50,7 +51,7 @@
                             else.</small-->
                     </div>
                 </div>
-                <div class="form-row col-12 mx-auto">
+                <div class="form-row col-12 mx-auto px-0">
                     <div class="form-group col-6">
                         <label class="row mx-auto my-0"for="exampleInputEmail1"><p class="text-danger m-0 mr-1">*</p>Estado</label>
                         <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
@@ -64,10 +65,11 @@
                             else.</small-->
                     </div>
                 </div>
+                <button class="btn btn-success ml-auto mr-1">Continuar</button>
             </form>
         </div>
     </div>
-    <div class="row col-3 border border-warning">
+    <div class="col-3 border border-warning">
         <div class="row mb-auto p-1 border">
             <div class="row col-12 mx-auto mt-1 mb-auto py-0 border-bottom">
                 <h4 class="col-12 mx-auto my-1 py-0 text-center text-primary">Resumen de compra</h4>
