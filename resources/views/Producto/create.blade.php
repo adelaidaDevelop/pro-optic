503eeeff404d8c5a7a54da4415be4c217c24d6cc
HeberZabdiel <hzhm1997@gmail.com>
2025-09-16 17:57:20 -0600

    Modificación Crear Producto

--- resources/views/Producto/create.blade.php
+++ resources/views/Producto/create.blade.php
@@ -1,152 +1,178 @@
 @extends('header2')
 @section('contenido')
 @section('subtitulo')
-PRODUCTOS
+    PRODUCTOS
 @endsection
 
 @section('opciones')
-<div class="col-0  p-1">
-    <form method="get" action="{{url('/puntoVenta/departamento/')}}">
-        <button class="btn btn-outline-secondary  ml-4 p-1 border-0" type="submit">
-            <img src="{{ asset('img\depto.svg') }}" alt="Editar" width="33px" height="33px">
-            <br />
-            <p class="h6 my-auto text-dark"><small>DEPARTAMENTOS</small></p>
-        </button>
-    </form>
-</div>
-<div class="col-6 "></div>
-<div class="my-auto">
-    <a class="btn btn-outline-secondary my-auto p-1 border-0" href="{{url('/puntoVenta/producto')}}">
-        <img src="{{ asset('img\anterior.png') }}" alt="Editar" width="35px" height="35px">
-    </a>
-</div>
-<div class=" ml-3 my-auto">
-    <a class="btn btn-outline-secondary my-auto p-1 border-0" href="{{url('/puntoVenta/venta')}}">
-        <img src="{{ asset('img\casa.png') }}" alt="Editar" width="35px" height="35px">
-    </a>
-</div>
+    <div class="col-0  p-1">
+        <form method="get" action="{{ url('/puntoVenta/departamento/') }}">
+            <button class="btn btn-outline-secondary  ml-4 p-1 border-0" type="submit">
+                <img src="{{ asset('img\depto.svg') }}" alt="Editar" width="33px" height="33px">
+                <br />
+                <p class="h6 my-auto text-dark"><small>DEPARTAMENTOS</small></p>
+            </button>
+        </form>
+    </div>
+    <div class="col-6 "></div>
+    <div class="my-auto">
+        <a class="btn btn-outline-secondary my-auto p-1 border-0" href="{{ url('/puntoVenta/producto') }}">
+            <img src="{{ asset('img\anterior.png') }}" alt="Editar" width="35px" height="35px">
+        </a>
+    </div>
+    <div class=" ml-3 my-auto">
+        <a class="btn btn-outline-secondary my-auto p-1 border-0" href="{{ url('/puntoVenta/venta') }}">
+            <img src="{{ asset('img\casa.png') }}" alt="Editar" width="35px" height="35px">
+        </a>
+    </div>
 @endsection
-
-<form method="post" action="{{url('/puntoVenta/producto')}}" enctype="multipart/form-data">
-    {{ csrf_field() }}
-
-    <div class="row my-auto">
-        <div class="text-primary p-1 my-auto ">
-            <strong class="ml-4 my-auto h5">
-                NUEVO PRODUCTO
-            </strong>
+<div class="row p-1 ">
+    <!--CONSULTAR PRODUCTO -->
+    <div class="row border border-dark m-2 w-100">
+        <div class="row col-12 mx-2 mt-2 mb-2">
+            <div class="text-primary p-1 my-auto ">
+                <strong class="ml-4 my-auto h5">
+                    NUEVO PRODUCTO
+                </strong>
+            </div>
         </div>
-    </div>
+        <form class="col-12" method="post" action="{{ url('/puntoVenta/producto') }}" enctype="multipart/form-data">
+            {{ csrf_field() }}
 
-    <div class="row  border border-dark m-1 w-100 ">
-        <div class="col-2"></div>
-        <div class="col-2 ">
-            <h6 class="mb-2 mt-3"> {{'CODIGO DE BARRAS'}}</h6>
-            <br />
-            <h6 class="mb-3 ">{{'NOMBRE'}}</h6>
-            <br />
-            <h6 class="mb-3"> {{'DESCRIPCION'}} </h6>
-            <br />
-            <h6 class="mb-4"> {{'MINIMO STOCK'}}</h6>
+            <div class="form-row border border-dark m-1 col-12">
+                <!-- INICIO-->
+                <div class="col-6 mr-auto">
+                    <div class="form-group mb-3">
+                        <label for="codigoBarras" class="form-label">CÓDIGO DE BARRAS</label>
+                        <input type="text" name="codigoBarras" id="codigoBarras" maxlength="20"
+                            class="form-control text-uppercase" placeholder="INGRESAR CÓDIGO DE BARRAS"
+                            value="{{ isset($producto->codigoBarras) ? $producto->codigoBarras : old('codigoBarras') }}"
+                            required autocomplete="codigoBarras" autofocus>
+                    </div>
 
-            <h6 class="mb-2"> {{'RECETA MEDICA'}} </h6>
-            <br />
-            <h6 class="mb-4"> {{'DEPARTAMENTO'}}</h6>
+                    <!-- Nombre -->
+                    <div class=" form-groupmb-3">
+                        <label for="nombre" class="form-label">NOMBRE</label>
+                        <input type="text" name="nombre" id="nombre" class="form-control"
+                            placeholder="NOMBRE DEL PRODUCTO"
+                            value="{{ isset($producto->nombre) ? $producto->nombre : old('nombre') }}" required>
+                    </div>
 
-            <h6 class="mb-4"> {{'EXISTENCIA'}}</h6>
+                    <!-- Descripción -->
+                    <div class=" form-groupmb-3">
+                        <label for="descripcion" class="form-label">DESCRIPCIÓN</label>
+                        <textarea name="descripcion" id="descripcion" class="form-control text-uppercase" placeholder="Descripción del producto"
+                            rows="2" required>{{ isset($producto->descripcion) ? $producto->descripcion : old('descripcion') }}</textarea>
+                    </div>
 
-            <h6 class="mb-4"> {{'COSTO'}}</h6>
+                    <!-- SPH -->
+                    <div class=" form-group mb-3">
+                        <label for="sph" class="form-label">SPH</label>
+                        @if (isset($producto))
+                            @foreach ($sucursalProd as $s)
+                                @if ($s->idProducto === $producto->id)
+                                    <input type="number" name="sph" id="sph" class="form-control"
+                                        min="1" value="{{ $s->minimoStock }}"
+                                        onkeypress="return validarEnteroPosi(event);" required>
+                                @endif
+                            @endforeach
+                        @else
+                            <input type="number" name="minimoStock" id="minimoStock" class="form-control"
+                                min="1" value="{{ old('minimoStock') }}" required>
+                        @endif
+                    </div>
 
-            <h6 class="mb-1"> {{'PRECIO'}}</h6>
-        </div>
-        <div class="col-4">
-            <!--El name debe ser igual al de la base de datos-->
-            <input type="text" name="codigoBarras" maxlength="20" id="codigoBarras" class="form-control text-uppercase mb-2 mt-3" placeholder="INGRESAR CODIGO DE BARRAS" value="{{ isset($producto->codigoBarras)?$producto->codigoBarras:old('codigoBarras')}}" required autocomplete="codigoBarras" autofocus>
-            <input type="text" name="nombre" id="nombre" class=" form-control mb-2 " placeholder="NOMBRE DEL PRODUCTO" value="{{ isset($producto->nombre)?$producto->nombre:old('nombre')}}" autofocus required>
-            <textarea name="descripcion" id="descripcion" class="text-uppercase form-control " placeholder="Descripcion del producto" rows="2" cols="23" required>{{ isset($producto->descripcion)?$producto->descripcion:old('descripcion')}}</textarea>
-            @if(isset($producto))
-            @foreach($sucursalProd as $sucursalProd)
-            @if($sucursalProd->idProducto === $producto->id)
-            <input type="number" name="minimoStock" min="1" id="minimoStock" class="form-control mb-2 mt-2" placeholder="Nombre productos" value="{{$sucursalProd->minimoStock}}" onkeypress=`return validarEnteroPosi(event);` autofocus required>
-            @endif
-            @endforeach
-            @else
-            <input type="number" name="minimoStock" min="1" id="minimoStock" class="form-control mb-2 mt-2" placeholder="MINIMO STOCK" value="{{old('minimoStock')}}" autofocus required>
-            @endif
+                    <!-- Mínimo stock -->
+                    <div class=" form-group mb-3">
+                        <label for="minimoStock" class="form-label">MÍNIMO STOCK</label>
+                        @if (isset($producto))
+                            @foreach ($sucursalProd as $s)
+                                @if ($s->idProducto === $producto->id)
+                                    <input type="number" name="minimoStock" id="minimoStock" class="form-control"
+                                        min="1" value="{{ $s->minimoStock }}"
+                                        onkeypress="return validarEnteroPosi(event);" required>
+                                @endif
+                            @endforeach
+                        @else
+                            <input type="number" name="minimoStock" id="minimoStock" class="form-control"
+                                min="1" value="{{ old('minimoStock') }}" required>
+                        @endif
+                    </div>
 
-            <select class="form-control text-uppercase" name="receta" id="receta" required>
-                <option value="">Seleccione</option>
-                @if(isset($producto))
-                @if( $producto->receta === "SI")
-                <option value="SI" selected> {{ $producto->receta }} </option>
-                <option value="NO"> NO </option>
-                @else
-                <option value="SI"> SI </option>
-                <option value="NO" selected>{{ $producto->receta }} </option>
-                @endif
-                @else
-                <option value="SI"> SI </option>
-                <option value="NO"> NO </option>
-                @endif
-            </select>
+                    <!-- Departamento -->
+                    <div class="mb-3">
+                        <label for="idDepartamento" class="form-label">DEPARTAMENTO</label>
+                        <select class="form-control text-uppercase" name="idDepartamento" id="idDepartamento" required>
+                            <option value="">Seleccione departamento</option>
+                            @foreach ($departamento as $d)
+                                <option value="{{ $d->id }}"
+                                    {{ isset($producto) && $producto->idDepartamento == $d->id ? 'selected' : '' }}>
+                                    {{ $d->nombre }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
 
-            <select class="form-control text-uppercase mb-2 mt-2" name="idDepartamento" id="idDepartamento" required>
-                <option value="">Seleccione departamento</option>
-                @foreach($departamento as $departamento)
-                @if(isset($producto))
-                @if( $producto->idDepartamento == $departamento->id)
-                <option value="{{ $departamento['id']}}" selected> {{$departamento['nombre']}}</option>
-                @else
-                <option value="{{ $departamento['id']}}"> {{$departamento['nombre']}}</option>
-                @endif
-                @else
-                <option value="{{ $departamento['id']}}"> {{$departamento['nombre']}}</option>
-                @endif
-                @endforeach
-            </select>
-            <input type="number" name="existencia" id="existencia" min="0" class=" form-control mb-2 " placeholder="INGRESAR EXISTENCIA" value="{{ old('existencia')}}" onkeypress="return validarEnteroPosi(event);"  autofocus required>
-            <input type="number" name="costo" min="0"  step="0.01" id="costo" class=" form-control mb-2 "  data-decimals="2" placeholder="INGRESAR COSTO" value="{{ old('costo')}}"  autofocus required>
-            <input type="number" name="precio" min="0"  step="0.01" id="precio" class=" form-control mb-4 " data-decimals="2" placeholder="INGRESAR PRECIO" value="{{old('precio')}}"  autofocus required>
-            
+                    <!-- Existencia -->
+                    <div class="mb-3">
+                        <label for="existencia" class="form-label">EXISTENCIA</label>
+                        <input type="number" name="existencia" id="existencia" min="0" class="form-control"
+                            placeholder="INGRESAR EXISTENCIA" value="{{ old('existencia') }}"
+                            onkeypress="return validarEnteroPosi(event);" required>
+                    </div>
+
+                    <!-- Costo -->
+                    <div class="mb-3">
+                        <label for="costo" class="form-label">COSTO</label>
+                        <input type="number" name="costo" id="costo" min="0" step="0.01"
+                            class="form-control" placeholder="INGRESAR COSTO" value="{{ old('costo') }}" required>
+                    </div>
+
+                    <!-- Precio -->
+                    <div class="mb-3">
+                        <label for="precio" class="form-label">PRECIO</label>
+                        <input type="number" name="precio" id="precio" min="0" step="0.01"
+                            class="form-control" placeholder="INGRESAR PRECIO" value="{{ old('precio') }}" required>
+                    </div>
+                </div>
+
+                <!-- FIN -->
+
+                <div class="col-3 text-center mt-3">
+                    <label for="imagen">
+                        <h5> <strong>{{ 'FOTO' }}</strong></h5>
+                    </label required>
+                    @if (isset($producto->imagen))
+                        <br />
+                        <img src="{{ asset('storage') . '/' . $producto->imagen }}" alt="" width="200">
+                    @endif
+                    @if (isset($producto->imagen))
+                        <input type="file" name="imagen" id="imagen" class="form-control" value="">
+                    @else
+                        <input class="form-control mb-4" type="file" name="imagen" id="imagen"
+                            value="" autofocus>
+                    @endif
+
+                    @error('mensajeError')
+                        <div class="alert alert-danger my-auto" role="alert">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                    @error('mensajeConf')
+                        <div class="alert alert-success my-auto" role="alert">
+                            {{ $message }}
+                        </div>
+                    @enderror
+
+                    <br /><br /><br /><br /><br /><br />
+                    <button class="btn btn-outline-secondary mt-4"
+                        onclick="return confirm('¿AGREGAR NUEVO PRODUCTO?')" type="submit" value="  AGREGAR">
+                        <img src="{{ asset('img\guardar.png') }}" class="img-thumbnail" alt="Editar"
+                            width="25px" height="25px"> GUARDAR PRODUCTO
+                    </button>
+                </div>
+            </div>
             <!--
-            <button class="btn btn-outline-secondary my-0 mb-1" onclick="return confirm('¿AGREGAR NUEVO PRODUCTO?')" type="submit" value="  AGREGAR">
-                <img src="{{ asset('img\guardar.png') }}" class="img-thumbnail" alt="Editar" width="25px" height="25px"> GUARDAR PRODUCTO
-            </button>
-            -->
-
-        </div>
-
-        <div class="col-3 text-center mt-3">
-            <label for="imagen">
-                <h5> <strong>{{'FOTO'}}</strong></h5>
-            </label required>
-            @if(isset($producto->imagen))
-            <br />
-            <img src="{{ asset('storage').'/'.$producto->imagen}}" alt="" width="200">
-            @endif
-            @if(isset($producto->imagen))
-            <input type="file" name="imagen" id="imagen" class="form-control" value="">
-            @else <input class="form-control mb-4" type="file" name="imagen" id="imagen" value="" autofocus>
-            @endif
-
-            @error('mensajeError')
-            <div class="alert alert-danger my-auto" role="alert">
-                {{$message}}
-            </div>
-            @enderror
-            @error('mensajeConf')
-            <div class="alert alert-success my-auto" role="alert">
-                {{$message}}
-            </div>
-            @enderror
-
-            <br /><br /><br /><br /><br /><br />
-            <button class="btn btn-outline-secondary mt-4" onclick="return confirm('¿AGREGAR NUEVO PRODUCTO?')" type="submit" value="  AGREGAR">
-                <img src="{{ asset('img\guardar.png') }}" class="img-thumbnail" alt="Editar" width="25px" height="25px"> GUARDAR PRODUCTO
-            </button>
-        </div>
-    </div>
-    <!--
     <div class="row text-right w-100">
         <div class="col-md-6"> </div>
         <div class="col-md-6">
@@ -161,8 +187,9 @@ PRODUCTOS
         </div>
     </div>
     -->
-</form>
-
+        </form>
+    </div>
+</div>
 
 <script>
     let ps = @json('sucursalProd');
@@ -209,7 +236,7 @@ PRODUCTOS
                 method: "post",
                 // la URL de donde voy a hacer la petición
                 //url: `/puntoVenta/productoSuc/actExistencia/${idSucProd}`,
-                url: `{{url('/puntoVenta/producto')}}`,
+                url: `{{ url('/puntoVenta/producto') }}`,
 
                 // los datos que voy a enviar para la relación
                 data: {
@@ -232,7 +259,7 @@ PRODUCTOS
             // $('#modal_precio_venta3').modal('hide');
             // $('#detalleProducto').modal('hide');
             alert("PRODUCTO AGREGADO CORRECTAMENTE");
-            location.href = "{{url('/puntoVenta/producto/')}}";
+            location.href = "{{ url('/puntoVenta/producto/') }}";
             //  refrescar();
         } catch (err) {
             console.log("Error al realizar la petición AJAX: " + err.message);
@@ -250,7 +277,7 @@ PRODUCTOS
                 (e.keyCode > 47 && e.keyCode < 58) ||
                 e.keyCode == 8 || e.keyCode == 46)) {
             return false;
-        } 
+        }
         return true;
     }
 
@@ -261,13 +288,13 @@ PRODUCTOS
             return false;
         }
     }
-   
+
     ///
     // var frase = "Son tres mil trescientos treinta y tres con nueve";
     // frase3 = frase.replace(/[aiou]/gi, 'e');
     //  alert(frase3);
 </script>
-<script src="{{asset('js\app.js')}}"></script>
+<script src="{{ asset('js\app.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.13/jquery.mask.min.js"></script>
 
 
